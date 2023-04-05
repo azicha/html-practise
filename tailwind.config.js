f94134111895e1900120244043f596558e731c99
azicha <askakauchi@gmail.com>
2023-04-05 15:58:22 +0300

    Add Custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito Sans', 'sans-serif'],
+        ubuntu: [ 'Ubuntu', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
