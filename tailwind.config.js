87704f6d60d227d0ad8589c91dfe193bf5479df4
azicha <askakauchi@gmail.com>
2023-04-09 22:31:01 +0300

    Add cuttly-html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html", "./src/flag/flag.html"],
+  content: ["./src/**/*.{html,js}", "index.html", "./src/flag/flag.html", ".src/cuttly-site/cuttly.html"],
   theme: {
     extend: {
       fontFamily: {
