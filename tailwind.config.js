f2ea3c6ec21d6e1e5c19703a0c4db61972237db2
azicha <askakauchi@gmail.com>
2023-04-11 16:45:01 +0300

    Add google.html path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html", "./src/flag/flag.html", ".src/cuttly-site/cuttly.html"],
+  content: ["./src/**/*.{html,js}", "index.html", "./src/flag/flag.html", ".src/cuttly-site/cuttly.html", ".src/input-boxes/google.html"],
   theme: {
     extend: {
       fontFamily: {
